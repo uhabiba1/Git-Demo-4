2d44d8a83a244a742f45005874297f1d8679eb55
uhabiba <u.habiba@uwinnipeg.ca>
2024-12-13 11:47:19 -0600

    updated background-color on App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#115e99',
     alignItems: 'center',
     justifyContent: 'center',
   },
